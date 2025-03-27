6b2416176b7e9a93855ad879943edc1927aeae82
EricEA <azayeveric@gmail.com>
2025-03-26 23:58:18 -0400

    Set up form for API calls

---
+++ HTML_Scraper/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import APIForm from "./assets/APIForm.jsx";
+
+import "./App.css";
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function App() {
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+  const reset = () => {
+    setInputs({
+      url: "",
+      format: "",
+      no_ads: "",
+      no_cookie_banners: "",
+      width: "",
+      height: "",
+    });
+  };
+  const [imageURL, setImageURL] = useState("");
+  const callAPI = async (query) => {
+    const respone = await fetch(query);
+    const json = await respone.json();
+    if (json.url == null) {
+      alert("Oops! Something went wrong with that query, let's try again!");
+    } else {
+      setCurrentImage(json.url);
+      reset();
+    }
+  };
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+    callAPI(query).catch(console.error);
+  };
+
+  const submitForm = () => {
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+      url: "",
+    };
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit an url!");
+    } else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value == "" || value == " ") {
+          inputs[key] = defaultValues[key];
+        }
+      }
+      makeQuery();
+    }
+  };
+
+  return (
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+    </div>
+  );
+}
+
+export default App;
